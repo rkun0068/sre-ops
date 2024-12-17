98c48dd2f24e65975a36ef681085d9dca0d82274
Yukun Song <yukun.song@shopee.com>
2024-12-17 16:54:24 +0800

    kodekloud/advanced_golang/concurrency

---
+++ kodekloud/advanced_golang/concurrency/sequential_program/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func calculateSquare(i int) {
+	time.Sleep(1 * time.Second)
+	fmt.Println(i * i)
+}
+
+func main() {
+	start := time.Now()
+
+	for i := 1; i < 10000; i++ {
+		calculateSquare(i)
+	}
+
+	elapsed := time.Since(start)
+	fmt.Println("Function took", elapsed)
+
+}
